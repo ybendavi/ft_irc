337541b8c67ca86cd5243b34b999665c5a95ce3a
Yohan BENDAVID <ybendavi@e1r6p18.clusters.42paris.fr>
2023-01-29 23:28:24 +0100

    messages parsé, début commande nick

--- Message.cpp
+++ Message.cpp
@@ -6,28 +6,28 @@
 /*   By: ccottin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 18:28:25 by ccottin           #+#    #+#             */
-/*   Updated: 2023/01/25 16:18:24 by ccottin          ###   ########.fr       */
+/*   Updated: 2023/01/29 23:26:18 by ybendavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Message.hpp"
+#include <iostream>
 
-Message::Message(void)
-{ }
+//Message::Message(void)
+//{ }
 
-Message::Message(const Message &ref)
-{ 
-	*this = ref;
-}
+//Message::Message(const Message &ref)
+//{ 
+//	*this = ref;
+//}
 
 Message::~Message(void)
 { } 
 
-Message::Message(const User &Sender, const User &Receiver,
-			const std::string content, const std::string command)
-	: _sender(Sender), _receiver(Receiver), _content(content),
-			_command(command)
-{ }
+Message::Message(User &sender, const std::string message): _sender(sender)
+{ 
+	parseMessage(message);
+}
 
 Message	&Message::operator=(const Message &ref)
 {
@@ -35,10 +35,91 @@ Message	&Message::operator=(const Message &ref)
 	{
 		this->_sender = ref._sender;
 		this->_receiver = ref._receiver;
-		this->_content = ref._content;
-		this->_command = ref.command;
+		this->_command = ref._command;
 	}
 	return (*this);
 }
 
+User&	Message::getSender(void) const { return (_sender); }
 
+std::string	Message::getParamsopt(void) const { return (_paramsopt); }
+
+std::string	Message::getMessage(void) const { return (_message); }
+
+std::string	Message::getCommand(void) const { return (_command); }
+
+std::vector<std::string>	Message::getParams(void) const { return (_params); }
+
+int	Message::parseMessage(const std::string message)
+{
+	size_t		pos;
+	size_t		param_pos;
+	size_t		last_pos;
+	std::string	tmp;
+	int		count;
+
+	count = 0;
+	if (message.empty() == true || message.size() > 512)
+		return (-1);
+	last_pos = message.find_first_of('\r');
+	if (last_pos == std::string::npos || message[last_pos + 1] != '\n')
+		return (-1);
+//	CMDS		//
+	if (message.find_first_of("QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm") != 0 && message.find_first_of("01234567890") != 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		if (message.find_first_of("0123456789") == 0 && message.find_first_not_of("0123456789") != 3)
+			return (-1);
+	}
+	std::cout << "1" << std::endl;
+//	PARAMS		//
+	if (message.find_first_of("QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm") == 0)
+		pos = message.find_first_not_of("QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm");
+	else
+		pos = message.find_first_not_of("0123456789");
+	if (pos < last_pos)
+		_command = message.substr(0, pos);
+	else
+		return (-1);
+	param_pos = pos;
+	if (message[pos] != ' ')
+		return (-1);
+	while ((param_pos = message.find_first_of(' ', param_pos)) != std::string::npos && message[param_pos + 1] != ':')
+	{
+		param_pos++;
+		count++;
+	}
+	if (count > 14)
+		return (-1);	
+	if (message[pos] == ' ')
+		pos++;
+//	RECEIVER	//
+	param_pos = pos;
+	while ( pos != std::string::npos && pos != message.find_first_of("\r\n:"))
+	{
+		param_pos = message.find_first_of(" \r\n:", param_pos);
+		if (param_pos <= last_pos)
+			_params.push_back(message.substr(pos, param_pos - pos));
+		if (param_pos != std::string::npos && param_pos != message.find_first_of("\r\n:"))
+			param_pos++;
+		pos = param_pos;
+	}
+	if (param_pos < last_pos && param_pos != std::string::npos && message[param_pos] == ':')
+	{
+		if (message.find_first_of("\n\r: ", param_pos + 1) != std::string::npos)
+			_paramsopt = message.substr(param_pos + 1,  message.find_first_of("\n\r: ", param_pos + 1) - param_pos - 1);
+
+	}
+	return (0);
+}
+
+char	*nick(Message& message)
+{
+	if (message.getParams().size() == 0)
+		return (ERRNONICKNAMEGIVEN);
+	message._sender.setNickname(*(message.getParams().begin()));
+
+}
--- Replies.hpp
+++ Replies.hpp
@@ -5,7 +5,13 @@
 # define RPL_YOURHOST "002"
 # define RPL_CREATED "003"
 # define RPL_MYINFO "004"
+//	NICK REPLIES	//
 # define ERR_NICKNAMEINUSE "433"
+# define ERR_NONICKNAMEGIVEN "431"
+# define ERR_ERRONEUSNICKNAME "432"
+# define ERR_NICKCOLLISION "436"
+# define ERR_UNAVAILRESOURCE "437"
+# define ERR_RESTRICTED "484"
 //"<nick> :Nickname is already in use"
 # define ERR_NEEDMOREPARAMS "461"
 //"<command> :Not enough parameters"
