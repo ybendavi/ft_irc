1ad4659fb8094a1324f7d6122fa406ffeb7da569
Curtis DAPURIFICACAO <cdapurif@e2r12p5.clusters.42paris.fr>
2023-02-08 17:58:41 +0100

    join ongoing

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -6,7 +6,7 @@
 /*   By: cdapurif <cdapurif@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 20:14:44 by cdapurif          #+#    #+#             */
-/*   Updated: 2023/02/08 15:06:41 by cdapurif         ###   ########.fr       */
+/*   Updated: 2023/02/08 17:27:43 by cdapurif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,37 @@ Channel::Channel(std::string name, unsigned short modes) : _channel(name), _chan
 Channel::~Channel(void)
 {}
 
+void    Channel::removeUserFromChannel(const std::string& nickname)
+{
+    _chanUsers.erase(nickname);
+}
+
+int    Channel::_checkUserPerms(const std::string& nickname)
+{
+    std::map<std::string, unsigned char>::iterator  it = _chanUsers.find(nickname);
+
+    if (it == _chanUsers.end())
+        return (0);
+    if (_chanMode & CHANBAN)
+    {
+        if (it->second & BAN)
+            return (1); //1 means user is blocked in this channel
+    }
+    return (0);
+}
+
 void    Channel::addUser(const std::string& user, unsigned char modes)
 {
-    _chanUsers[user] = modes;
+    int status;
+
+    status = _checkUserPerms(user);
+    if (status == 0)
+        _chanUsers[user] = modes;
+    else
+    {
+        if (status == 1)
+            std::cout << "user " << user << " is banned from channel " << _channel << std::endl;
+        else
+            std::cout << "special case when adding user " << user << " to channel " << _channel << std::endl;
+    }
 }
